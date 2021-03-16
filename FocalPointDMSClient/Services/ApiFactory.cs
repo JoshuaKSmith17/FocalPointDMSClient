a43f0a304c2e5d8646f68c1b1060bfcfab2bdfde
joshua Smith <joshuaksmith17@gmail.com>
2021-03-15 19:51:35 -0500

    Created factory for creation of different data access services.  Designed to provide creation functionality for other object types we want to access.

---
+++ FocalPointDMSClient/Services/ApiFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FocalPointDMSClient.Services
+{
+    public class ApiFactory
+    {        
+        public IApiServiceStrategy GetCustomerStrategy()
+        {
+            return new CustomerServices();
+        }
+    }
+}
