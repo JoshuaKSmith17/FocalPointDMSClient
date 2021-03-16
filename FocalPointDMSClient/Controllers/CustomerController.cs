aad5b7876dcd17f5556046f41a12bb589884d9e8
joshua Smith <joshuaksmith17@gmail.com>
2021-03-16 17:54:30 -0500

    Created Controller class to handles details the customercommandclass was handling.

---
+++ FocalPointDMSClient/Controllers/CustomerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows;
+
+using FocalPointDMSClient.Models;
+using FocalPointDMSClient.Services;
+
+
+namespace FocalPointDMSClient.Controllers
+{
+    class CustomerController
+    {
+        DataTable DataTable;
+        public CustomerController(DataTable dataTable)
+        {
+            this.DataTable = dataTable;
+            dataTable.Clear();
+        }
+        public DataTable BuildTable()
+        {
+            var factory = (ApiFactory) Application.Current.Resources["apiFactory"];
+            IApiServiceStrategy service = factory.GetCustomerStrategy();
+            service.QueryAllItems();
+            var customers = (Customer[]) service.GetAllItems();
+
+            DataTable = new DataTable();
+
+            DataColumn dataColumn;
+            DataRow dataRow;
+
+            dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.Int64");
+            dataColumn.ColumnName = "id";
+            dataColumn.ReadOnly = true;
+            DataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.ColumnName = "Name";
+            dataColumn.ReadOnly = true;
+            DataTable.Columns.Add(dataColumn);
+
+            foreach (var customer in customers)
+            {
+                dataRow = DataTable.NewRow();
+                dataRow["id"] = customer.Id;
+                dataRow["Name"] = customer.Name;
+                DataTable.Rows.Add(dataRow);
+            }
+
+            return DataTable;
+        }
+    }
+}
--- FocalPointDMSClient/ViewModels/GetCustomersCommand.cs
+++ FocalPointDMSClient/ViewModels/GetCustomersCommand.cs
@@ -1,9 +1,8 @@
-﻿using FocalPointDMSClient.Models;
-using FocalPointDMSClient.Services;
-using System;
-using System.Data;
+﻿using System;
 using System.Windows;
 using System.Windows.Input;
+
+using FocalPointDMSClient.Controllers;
 
 namespace FocalPointDMSClient.ViewModels
 {
@@ -21,40 +20,10 @@ namespace FocalPointDMSClient.ViewModels
 
         public void Execute(object parameter)
         {
-            ApiFactory apiFactory = (ApiFactory) Application.Current.Resources["apiFactory"];
-            IApiServiceStrategy service = apiFactory.GetCustomerStrategy();
-            service.QueryAllItems();
-            var customers = (Customer[]) service.GetAllItems();
-            DataTable dataTable = new DataTable();
-
-            DataColumn dataColumn;
-            DataRow dataRow;
-
-            dataColumn = new DataColumn();
-            dataColumn.DataType = System.Type.GetType("System.Int64");
-            dataColumn.ColumnName = "id";
-            dataColumn.ReadOnly = true;
-            dataTable.Columns.Add(dataColumn);
-
-            dataColumn = new DataColumn();
-            dataColumn.DataType = System.Type.GetType("System.String");
-            dataColumn.ColumnName = "Name";
-            dataColumn.ReadOnly = true;
-            dataTable.Columns.Add(dataColumn);
-
-            foreach (var customer in customers)
-            {
-                dataRow = dataTable.NewRow();
-                dataRow["id"] = customer.Id;
-                dataRow["Name"] = customer.Name;
-                dataTable.Rows.Add(dataRow);
-            }
-
             MainViewModel mainViewModel = (MainViewModel)Application.Current.Resources["mainViewModel"];
-            mainViewModel.MainDataTable = dataTable;
-
-            mainViewModel.StatusTextOutput += customers.Length + " Customers Loaded\n";
-
+            CustomerController controller = new CustomerController(mainViewModel.MainDataTable);
+            mainViewModel.MainDataTable = controller.BuildTable();
+            mainViewModel.StatusTextOutput += mainViewModel.MainDataTable.Rows.Count + " Customers Loaded\n";
 
         }
 
