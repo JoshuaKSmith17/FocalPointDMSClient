1e459c90fc308f4f2f67fe6094dae6b8b05770ed
joshua Smith <joshuaksmith17@gmail.com>
2021-03-15 19:50:30 -0500

    Created an interface for querying customers to the database and a service class to go with it.

---
+++ FocalPointDMSClient/Services/IApiServiceStrategy.cs
@@ -0,0 +1,13 @@
+﻿using FocalPointDMSClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FocalPointDMSClient.Services
+{
+    public interface IApiServiceStrategy
+    {
+        public void QueryAllItems();
+        public IDbObject[] GetAllItems();
+    }
+}
