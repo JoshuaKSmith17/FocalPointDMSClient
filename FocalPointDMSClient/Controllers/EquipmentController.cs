0d5b553010ac90d197b6c177552188da3af52eed
joshua Smith <joshuaksmith17@gmail.com>
2021-03-16 18:25:24 -0500

    Created EquipmentController class to handle building the datatable for an Equipment query.

---
+++ FocalPointDMSClient/Controllers/EquipmentController.cs
@@ -0,0 +1,63 @@
+﻿using System.Data;
+using System.Windows;
+
+using FocalPointDMSClient.Models;
+using FocalPointDMSClient.Services;
+
+namespace FocalPointDMSClient.Controllers
+{
+    public class EquipmentController : IDataTableProvider
+    {
+        DataTable DataTable;
+        public EquipmentController()
+        {
+            this.DataTable = new DataTable();
+        }
+        public DataTable BuildTable()
+        {
+            var factory = (ApiFactory)Application.Current.Resources["apiFactory"];
+            IApiServiceStrategy service = factory.GetEquipmentStrategy();
+            service.QueryAllItems();
+            var items = (Equipment[])service.GetAllItems();
+
+            DataColumn dataColumn;
+            DataRow dataRow;
+
+            dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.Int64");
+            dataColumn.ColumnName = "id";
+            dataColumn.ReadOnly = true;
+            DataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.ColumnName = "Make";
+            dataColumn.ReadOnly = true;
+            DataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.ColumnName = "Model";
+            dataColumn.ReadOnly = true;
+            DataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.ColumnName = "Serial Number";
+            dataColumn.ReadOnly = true;
+            DataTable.Columns.Add(dataColumn);
+
+            foreach (var equipment in items)
+            {
+                dataRow = DataTable.NewRow();
+                dataRow["id"] = equipment.Id;
+                dataRow["Make"] = equipment.Make;
+                dataRow["Model"] = equipment.Model;
+                dataRow["Serial Number"] = equipment.SerialNumber;
+                DataTable.Rows.Add(dataRow);
+            }
+
+            return DataTable;
+        }
+    }
+}
