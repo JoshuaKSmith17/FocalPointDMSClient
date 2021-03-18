8b20cd2efafded6649671ed3da336075e4f1e752
joshua Smith <joshuaksmith17@gmail.com>
2021-03-18 17:33:12 -0500

    New factory for fabricating controller classes.

---
+++ FocalPointDMSClient/Controllers/ControllerFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+using FocalPointDMSClient.Models.OrmModels;
+
+namespace FocalPointDMSClient.Controllers
+{
+    class ControllerFactory
+    {
+        public Dictionary<EntityType, Func<IDataTableProvider>> serviceCreators { get; private set; }
+        public ControllerFactory()
+        {
+            serviceCreators = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => typeof(IDataTableProvider).IsAssignableFrom(t) && t.IsInterface == false)
+                .Select(t => new Func<IDataTableProvider>(() => Activator.CreateInstance(t) as IDataTableProvider))
+                .ToDictionary(f => f().EntityType);
+        }
+
+        public IDataTableProvider GetInstance(EntityType entityType)
+        {
+            return serviceCreators[entityType]();
+        }
+    }
+}
