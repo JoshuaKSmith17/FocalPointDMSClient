fac83ff9c195776b86dbf3f60b76c19133b05f37
joshua Smith <joshuaksmith17@gmail.com>
2021-03-18 18:33:58 -0500

    Added classes for viewing a customer object in detail.

---
+++ FocalPointDMSClient/ViewModels/GetCustomerCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+using FocalPointDMSClient.Controllers;
+using FocalPointDMSClient.Models.OrmModels;
+using FocalPointDMSClient.Views;
+
+namespace FocalPointDMSClient.ViewModels
+{
+    class GetCustomerCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            CustomerDetailViewModel viewModel = new CustomerDetailViewModel();
+            CustomerDetail customerDetail = new CustomerDetail();
+            //var mainViewModel = (MainViewModel)Application.Current.Properties["mainViewModel"];
+            //var factory = (ControllerFactory)Application.Current.Properties["controllerFactory"];
+            //var controller = factory.GetInstance(EntityType.Customer);
+            //mainViewModel.MainDataTable = controller.BuildTable();
+            //mainViewModel.StatusTextOutput += mainViewModel.MainDataTable.Rows.Count + " Customers Loaded\n";
+
+        }
+    }
+}
