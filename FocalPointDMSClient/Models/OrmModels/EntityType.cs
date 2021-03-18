2743e35a94ff68350db5e5b68d59cb63b31eda04
joshua Smith <joshuaksmith17@gmail.com>
2021-03-17 21:47:38 -0500

    Added EntityType that assists ApiFactory with loading each service object.

---
+++ FocalPointDMSClient/Models/OrmModels/EntityType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FocalPointDMSClient.Models.OrmModels
+{
+    public enum EntityType
+    {
+        Customer,
+        Equipment
+    }
+}
