48de93dd803e8ed753a1af1e97301aae6b0fa977
joshua Smith <joshuaksmith17@gmail.com>
2021-03-18 18:34:20 -0500

    Expanded dataTable to display all customer fields.

--- FocalPointDMSClient/Controllers/CustomerController.cs
+++ FocalPointDMSClient/Controllers/CustomerController.cs
@@ -40,11 +40,39 @@ namespace FocalPointDMSClient.Controllers
             dataColumn.ReadOnly = true;
             DataTable.Columns.Add(dataColumn);
 
+            dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.ColumnName = "Address";
+            dataColumn.ReadOnly = true;
+            DataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.ColumnName = "City";
+            dataColumn.ReadOnly = true;
+            DataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.String");
+            dataColumn.ColumnName = "State";
+            dataColumn.ReadOnly = true;
+            DataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn();
+            dataColumn.DataType = System.Type.GetType("System.Int32");
+            dataColumn.ColumnName = "Zip Code";
+            dataColumn.ReadOnly = true;
+            DataTable.Columns.Add(dataColumn);
+
             foreach (var customer in customers)
             {
                 dataRow = DataTable.NewRow();
                 dataRow["id"] = customer.Id;
-                dataRow["Name"] = customer.Name;
+                dataRow["Name"] = customer.Name;                
+                dataRow["Address"] = customer.Address; 
+                dataRow["City"] = customer.City; 
+                dataRow["State"] = customer.State;
+                dataRow["Zip Code"] = customer.ZipCode;
                 DataTable.Rows.Add(dataRow);
             }
 
