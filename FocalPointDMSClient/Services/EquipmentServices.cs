0cf4147651093b780dde0f6b7e5be5562dd0146a
joshua Smith <joshuaksmith17@gmail.com>
2021-03-18 17:32:53 -0500

    Changed the name of class to follow the naming convention of my other services classes.  I honestly don't know how I missed this.

---
+++ FocalPointDMSClient/Services/EquipmentServices.cs
@@ -0,0 +1,34 @@
+﻿using FocalPointDMSClient.Models;
+using FocalPointDMSClient.Models.OrmModels;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace FocalPointDMSClient.Services
+{
+    class EquipmentServices : IApiServiceStrategy
+    {
+        HttpClient Client;
+        Equipment[] Items;
+        public EntityType EntityType => EntityType.Equipment;
+
+        public EquipmentServices()
+        {
+            Client = new HttpClient();
+        }
+        public async void QueryAllItems()
+        {
+            Client.BaseAddress = new Uri("https://localhost:44345/api/");
+
+            var responseTask = Client.GetAsync("Equipment");
+            responseTask.Wait();
+            Items = await responseTask.Result.Content.ReadAsAsync<Equipment[]>();
+        }
+
+        public IDbObject[] GetAllItems()
+        {
+            return Items;
+        }
+    }
+}
