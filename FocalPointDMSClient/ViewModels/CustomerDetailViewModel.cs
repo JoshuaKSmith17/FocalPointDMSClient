3d32021e255019e2b422ffd0a5eb3a8561bfedfd
joshua Smith <joshuaksmith17@gmail.com>
2021-03-22 20:06:26 -0500

    CustomerDetail view and viewmodel initial commit.  Will be used for updating a customer record in the web server database.

--- FocalPointDMSClient/ViewModels/CustomerDetailViewModel.cs
+++ FocalPointDMSClient/ViewModels/CustomerDetailViewModel.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Data;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using FocalPointDMSClient.Models.OrmModels;
 
 namespace FocalPointDMSClient.ViewModels
 {
-    class CustomerDetailViewModel
+    class CustomerDetailViewModel : INotifyPropertyChanged
     {
+        private Customer customer;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Customer Customer
+        {
+            get { return customer; }
+            set
+            {
+                customer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public CustomerDetailViewModel(Customer customer)
+        {
+            this.Customer = customer;
+        }
     }
 }
