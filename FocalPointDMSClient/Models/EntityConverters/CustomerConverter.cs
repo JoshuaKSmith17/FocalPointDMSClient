261b9d9d09b5c0ff94004f626b0710729cce57b4
joshua Smith <joshuaksmith17@gmail.com>
2021-03-23 19:38:18 -0500

    Added converter class to convert a DataRowView object back into its original class.

---
+++ FocalPointDMSClient/Models/EntityConverters/CustomerConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+using FocalPointDMSClient.Models.OrmModels;
+
+namespace FocalPointDMSClient.Models.EntityConverters
+{
+    class CustomerConverter
+    {
+        private DataRowView DataRow;
+
+        public CustomerConverter(DataRowView dataRow)
+        {
+            DataRow = dataRow;
+        }
+
+        public Customer Convert()
+        {
+            Customer customer = new Customer();
+
+            customer.Id = (long)DataRow["Id"];
+            customer.Name = (string)DataRow["Name"];
+            customer.Address = (string)DataRow["Address"];
+            customer.City = (string)DataRow["City"];
+            customer.State = (string)DataRow["City"];
+            customer.ZipCode = (int)DataRow["Zip Code"];
+
+            return customer;
+        }
+    }
+}
