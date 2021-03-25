d74503dcfda9c1551d25ed4dd0b22f221578b2a8
joshua Smith <joshuaksmith17@gmail.com>
2021-03-24 20:34:29 -0500

    Created and implemented the command to update a customer and configured the CustomerDetail view to call it.

---
+++ FocalPointDMSClient/ViewModels/UpdateCustomerCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+using FocalPointDMSClient.Models.DataTableBuilders;
+using FocalPointDMSClient.Models.OrmModels;
+using FocalPointDMSClient.Controllers;
+
+namespace FocalPointDMSClient.ViewModels
+{
+    class UpdateCustomerCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+
+            var input = (Customer)parameter;
+            CustomerController controller = new CustomerController();
+            controller.UpdateRecord(input);            
+
+            var viewModel = (MainViewModel)Application.Current.Properties["mainViewModel"];
+            viewModel.MainDataTable = controller.BuildTable();
+            viewModel.StatusTextOutput += viewModel.MainDataTable.Rows.Count + " Customers Loaded\n";
+        }
+    }
+}
