5e3c2fbac9dc8e5e05719798bedb419639d2efd6
joshua Smith <joshuaksmith17@gmail.com>
2021-03-24 19:05:52 -0500

    Implemented new CustomerController class and a corresponding interface.

---
+++ FocalPointDMSClient/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Windows;
+
+using FocalPointDMSClient.Models.OrmModels;
+using FocalPointDMSClient.Models.DataTableBuilders;
+using FocalPointDMSClient.Services;
+
+
+namespace FocalPointDMSClient.Controllers
+{
+    class CustomerController : IController
+    {
+        public DataTable BuildTable()
+        {
+            var factory = (DataTableBuilderFactory)Application.Current.Properties["DataTableBuilderFactory"];
+            var tableBuilder = factory.GetInstance(EntityType.Customer);
+
+            var dataTable = tableBuilder.BuildTable();
+            return dataTable;
+        }
+
+        public void UpdateRecord(IDbObject item)
+        {
+            var factory = (ApiFactory)Application.Current.Properties["apiFactory"];
+            IApiServiceStrategy service = factory.GetInstance(EntityType.Customer);
+
+            service.PutItem(item);
+        }
+    }
+}
