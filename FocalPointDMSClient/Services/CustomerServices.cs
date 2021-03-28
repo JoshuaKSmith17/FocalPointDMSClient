b654148bd2e6d4d112048fb3ad5d7613e519f78f
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-03-28 13:45:47 -0500

    Implemented DeleteItem for the CustomerServices class.

--- FocalPointDMSClient/Services/CustomerServices.cs
+++ FocalPointDMSClient/Services/CustomerServices.cs
@@ -52,6 +52,13 @@ namespace FocalPointDMSClient.Services
             var responseTask = await Client.PostAsync("Customers/", byteContent);
         }
 
+        public async void DeleteItem(DbObject item)
+        {
+            Client.BaseAddress = new Uri("https://localhost:44345/api/");
+
+            var responseTask = await Client.DeleteAsync("Customers/" + item.Id);
+        }
+
         public DbObject[] GetAllItems()
         {
             return Items;
