3a7a72cb79b960f8eb93852b51cc2bb04a764f7d
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-03-30 18:11:49 -0500

    Created EquipmentMainViewModel.

---
+++ FocalPointDMSClient/ViewModels/MainView/EquipmentVm/EquipmentMainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+using FocalPointDMSClient.Models.OrmModels;
+
+namespace FocalPointDMSClient.ViewModels.MainView.EquipmentVm
+{
+    class EquipmentMainViewModel : MainViewModel
+    {
+        public EquipmentMainViewModel() : base()
+        {
+
+            EntityType = EntityType.Equipment;
+
+            // Sets the MainViewModel application property to this particular instance.
+            Application.Current.Properties["MainViewModel"] = this;
+        }
+    }
+}
