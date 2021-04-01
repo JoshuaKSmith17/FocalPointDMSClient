ecf7bb89f9873623c1d12de26f87e53476bda6c2
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-04-01 17:13:00 -0500

    Initial setup of the EquipmentDataTableBuilderTester.

---
+++ FocalPointDMSClient.Tests/EquipmentDataTableBuilderTester.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+using FocalPointDMSClient.Models.OrmModels;
+using FocalPointDMSClient.Models.DataTableBuilders;
+
+namespace FocalPointDMSClient.Tests
+{
+    class EquipmentDataTableBuilderTester
+    {
+        private EquipmentDataTableBuilder _equipmentDataTableBuilder;
+        private Equipment[] _equipments;
+
+        [SetUp]
+        public void Setup()
+        {
+            _equipments = new Equipment[2];
+            _equipments[0] = new Equipment();
+            _equipments[1] = new Equipment()
+            {
+                Id = 1,
+                Make = "Test",
+                Model = "Test",
+                SerialNumber = "Test"
+            };
+        }
+    }
+}
