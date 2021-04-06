df0e8780d2b275faaf7432d46277a4890fdc3b4c
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-04-05 21:24:22 -0500

    Implemented new command to handle a delete equipment command from the user.

---
+++ FocalPointDMSClient/ViewModels/MainView/EquipmentVm/DeleteEquipmentCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+using FocalPointDMSClient.Controllers;
+using FocalPointDMSClient.Models.EntityConverters;
+using FocalPointDMSClient.Models.OrmModels;
+
+namespace FocalPointDMSClient.ViewModels.MainView.EquipmentVm
+{
+    class DeleteEquipmentCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            var mainViewModel = (MainViewModel)Application.Current.Properties["mainViewModel"]; 
+
+            var customer = new Equipment(mainViewModel.SelectedItemRow);
+
+            QueryController controller = new QueryController(EntityType.Equipment);
+            controller.DeleteRecord(customer);
+        }
+    }
+}
+
