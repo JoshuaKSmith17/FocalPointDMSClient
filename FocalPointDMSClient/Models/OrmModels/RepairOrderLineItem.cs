eb38b37ea60df29357da82ab69bf4eeaf7cfd697
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-04-06 17:57:04 -0500

    Created RepairOrderLineItem class.

---
+++ FocalPointDMSClient/Models/OrmModels/RepairOrderLineItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FocalPointDMSClient.Models.OrmModels
+{
+    public class RepairOrderLineItem
+    {
+        public long Id { get; set; }
+        public string WorkPerformed { get; set; }
+        public Equipment Equipment { get; set; }
+    }
+}
