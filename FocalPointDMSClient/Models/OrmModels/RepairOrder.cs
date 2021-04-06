5a10f27fdd589d1b629a4c4ccfdb354771b57175
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-04-06 17:56:53 -0500

    Created RepairOrder class.

---
+++ FocalPointDMSClient/Models/OrmModels/RepairOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FocalPointDMSClient.Models.OrmModels
+{
+    public class RepairOrder
+    {
+        public long Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime CompletedData { get; set; }
+        public List<RepairOrderLineItem> RepairOrderLineItems { get; set; }
+    }
+}
