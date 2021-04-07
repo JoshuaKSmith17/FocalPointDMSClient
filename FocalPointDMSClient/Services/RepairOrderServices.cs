e4c159ea5704022a7a26da4b6e682a346f0d8de3
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-04-06 19:03:57 -0500

    Added RepairOrderServices.cs and set up the unimplemented methods.

---
+++ FocalPointDMSClient/Services/RepairOrderServices.cs
@@ -0,0 +1,46 @@
+﻿using FocalPointDMSClient.Models.OrmModels;
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FocalPointDMSClient.Services
+{
+    public class RepairOrderServices : IApiServiceStrategy
+    {
+        HttpClient Client;
+        RepairOrder[] Items;
+        public EntityType EntityType => EntityType.RepairOrder;
+
+        public RepairOrderServices()
+        {
+            Client = new HttpClient();
+        }
+
+        public async Task<DbObject[]> QueryAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<HttpResponseMessage> PutItem(DbObject item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<HttpResponseMessage> CreateItem(DbObject item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<HttpResponseMessage> DeleteItem(DbObject item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbObject[] GetAllItems()
+        {
+            return Items;
+        }
+    }
+}
